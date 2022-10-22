563ae595b943d4e2b2d44defe28bba3d08affffc
shane moran <shane.c.moran@gmail.com>
2022-10-22 11:38:53 -0400

    init

--- src/main/java/com/scmtf/Main.java
+++ src/main/java/com/scmtf/Main.java
@@ -2,6 +2,11 @@ package com.scmtf;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Trying to print secret contents of test.txt!");
+
+        /*
+         SOME CODE TO PRINT OUT THE CONTENTS OF src/dist/exampleDir/test.txt
+        that will work in both gradle and when ran by user from the disTar archive
+         */
     }
 }
