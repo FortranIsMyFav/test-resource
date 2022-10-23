d4f2e6e8d49beaa7aba35b3c6f0208c759733449
shane moran <shane.c.moran@gmail.com>
2022-10-23 08:48:58 -0400

    init

--- src/main/java/com/scmtf/Main.java
+++ src/main/java/com/scmtf/Main.java
@@ -2,11 +2,14 @@ package com.scmtf;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Trying to print secret contents of test.txt!");
+        System.out.println("Trying to get to files in src/dist/orekit-data");
 
         /*
-         SOME CODE TO PRINT OUT THE CONTENTS OF src/dist/exampleDir/test.txt
-        that will work in both gradle and when ran by user from the disTar archive
-         */
+        TODO: Add code to do the following
+        - Create a File() object on src/dist/orekit-data
+        - read contents of test.txt
+        - read contents of userCSV.csv
+        */
+
     }
 }
