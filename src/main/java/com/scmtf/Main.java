a84cc33ccab34addbc0aef7a3b5b28b9bc2cf0ed
shane moran <shane.c.moran@gmail.com>
2022-10-24 11:28:02 -0400

    init

--- src/main/java/com/scmtf/Main.java
+++ src/main/java/com/scmtf/Main.java
@@ -1,9 +1,16 @@
 package com.scmtf;
 
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Trying to get to files in src/dist/orekit-data");
+        System.out.println("Current paths on CLASSPATH:");
 
+        String classpath = System.getProperty("java.class.path");
+        String[] classPathValues = classpath.split(File.pathSeparator);
+        for (String classPath: classPathValues) {
+            System.out.println(classPath);
+        }
         /*
         TODO: Add code to do the following
         - Create a File() object on src/dist/orekit-data
